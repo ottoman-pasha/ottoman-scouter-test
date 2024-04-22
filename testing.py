f1762772771c11a42635c499af63f87213348ed9
ottoman-pasha <167790902+ottoman-pasha@users.noreply.github.com>
2024-04-22 19:47:49 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     websocket_thread = threading.Thread(target=websocket_thread)
     websocket_thread.daemon = True
     websocket_thread.start()
-    app.run(debug=False)
+    app.run(debug=True)
 
   
 
